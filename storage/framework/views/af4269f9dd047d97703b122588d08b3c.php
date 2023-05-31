935ffbc179ef69e7511f8ece093eb6ec6c1b858f
Oakar Kyaw <107309612+Oakar-Kyaw@users.noreply.github.com>
2023-05-31 12:34:27 +0630

    Add files via upload

---
+++ storage/framework/views/af4269f9dd047d97703b122588d08b3c.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello This is cast</h1>
+</body>
+</html><?php /**PATH D:\laravel-10-for-beginner\resources\views/lara.blade.php ENDPATH**/ ?>
